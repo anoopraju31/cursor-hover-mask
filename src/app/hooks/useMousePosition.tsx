9cb4e152d89e4f8341579c885a2753946a44922a
anoopraju31 <anoopraju31@gmail.com>
2023-12-23 17:11:57 +0530

    Custom Hook - useMousePosition - refactor

--- src/app/hooks/useMousePosition.tsx
+++ src/app/hooks/useMousePosition.tsx
@@ -11,37 +11,18 @@ const useMousePosition = () => {
 		y: -1000,
 	})
 	const containerRef = useRef<HTMLDivElement | null>(null)
-	const mousePositionWithRespectToScreen = useRef<MousePosition>({
-		x: -1000,
-		y: -1000,
-	})
-
-	const handleMousePosition = (e: MouseEvent) => {
-		setMousePosition({ x: e.pageX, y: e.pageY })
-		mousePositionWithRespectToScreen.current = {
-			x: e.clientX,
-			y: e.clientY,
-		}
-	}
-
-	const handleMouseLeave = (e: MouseEvent) => {
-		setMousePosition({
-			x: e.clientX,
-			y: window.innerHeight * 2,
-		})
-	}
 
 	useEffect(() => {
-		// const container = containerRef.current
-
-		// if (!container) return
-
+		const handleMousePosition = (e: MouseEvent) => {
+			setMousePosition({
+				x: e.pageX,
+				y: e.pageY,
+			})
+		}
 		document.addEventListener('mousemove', handleMousePosition)
-		// document.addEventListener('mouseleave', handleMouseLeave)
 
 		return () => {
 			document.removeEventListener('mousemove', handleMousePosition)
-			// document.removeEventListener('mouseleave', handleMouseLeave)
 		}
 	}, [])
 
