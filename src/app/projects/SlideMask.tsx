24474c30d814ffe8a3355e6116867fe44d19552c
anoopraju31 <anoopraju31@gmail.com>
2023-12-24 10:28:30 +0530

    Project Mask - refactor

--- src/app/projects/SlideMask.tsx
+++ src/app/projects/SlideMask.tsx
@@ -17,9 +17,7 @@ const SlideMask = (props: MaskSlideProps) => {
 		<div className='w-full px-[10px] h-screen flex justify-center items-center sticky top-0'>
 			<motion.div
 				style={{ top: `calc(-0% + ${id * 35}px)`, scale }}
-				className='w-full h-[60%] relative rounded-2xl'>
-				<div className='w-full h-full'></div>
-			</motion.div>
+				className='w-full h-[60%] relative rounded-2xl'></motion.div>
 		</div>
 	)
 }
